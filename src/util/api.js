8d84004657162262ae7d04f5d3b87522515c05a9
Edward Miller <edmiller.me@gmail.com>
2022-03-30 12:42:49 +1300

    Update api.js

--- src/util/api.js
+++ src/util/api.js
@@ -1,6 +1,6 @@
 /*
   A function that sets up a connection to a graphql API endpoint. 
-  It has a query parameter that will allow an functions to query the specific data it needs
+  It has a query parameter that will allow any functions to query the specific data it needs
   as well as use any variables that it may require as well
 */
 export const getApi = (query, variables) => {
